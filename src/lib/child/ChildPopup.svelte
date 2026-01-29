e5aec258466119b86ac801f3ddee809513c8feac
Amal B Nair <amal004nair@gmail.com>
2026-01-29 19:23:40 +0530

    Initial Commit by ABN Added the primary round interview codes also. For future references

---
+++ src/lib/child/ChildPopup.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let skill_name = ""
+    export let experience = ""
+    export let is_open = false
+
+    function handle_cancel(){
+        is_open = false
+    }
+</script>
+
+<dialog>
+    <div class="form_container">
+        <div class="inputs">
+            <input type="text" placeholder="Skill name" bind:value={skill_name}>
+            <input type="text" placeholder="No of Years Experience" bind:value={experience}>
+        </div>
+        <div class="button_container">
+            <button on:click>save</button>
+            <button on:click={handle_cancel}>cancel</button>
+        </div>
+    </div>
+
+
+</dialog>
+
+
+
+<style>
+    dialog  {
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #fff;
+        border-radius: 10px;
+        padding: 20px;
+    }
+
+    .form_container{
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        height: 400px;
+        width: 400px;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,131 @@
+<!--Interview Done On 26/01/2026 7.30 pm -->
+
+
+<script>
+    import ChildPopup from '$lib/child/ChildPopup.svelte';
+
+    let skill_name = ''
+    let experience = ''
+
+    let is_open = false
+    let skills = [
+        {
+            skill_name : "Svelte ",
+            experience : "3"
+        },
+        {
+            skill_name : "React ",
+            experience : "2"
+        },
+
+    ]
+
+    function handle_open_skill_popup(){
+        is_open = true
+    }
+
+    function handle_save_skill(){
+        skills.push({
+            skill_name,
+            experience
+        })
+
+        skills = [...skills]
+
+        experience = ""
+        skill_name = ""
+
+        is_open = false
+    }
+
+</script>
+
+
+<div class="main_container">
+
+
+
+    {#if is_open}
+        <ChildPopup
+                bind:skill_name
+                bind:experience
+                bind:is_open
+                on:click={handle_save_skill}
+        />
+    {/if}
+
+    <div class="man_skills_container">
+        {#if skills.length}
+            <table>
+                <thead>
+                    <tr>
+                        <td>Skill Name</td>
+                        <td>Experience</td>
+                    </tr>
+                </thead>
+                <tbody>
+                {#each skills as individual_skills}
+                <tr>
+                    <td>{individual_skills.skill_name}</td>
+                    <td>{individual_skills.experience}</td>
+                </tr>
+                {/each}
+                </tbody>
+            </table>
+            <div class="button_container">
+                <button class="add_skills" on:click={handle_open_skill_popup}>
+                    Add a skill
+                </button>
+            </div>
+
+<!--            -->
+<!--            -->
+<!--            -->
+<!--            {#each skills as individual_skills}-->
+<!--                <div class="skill_container">-->
+<!--                    <h5>Skill Name : {individual_skills.skill_name}</h5>-->
+<!--                    <p>Experience : {individual_skills.experience}</p>-->
+<!--                </div>-->
+<!--            {/each}-->
+        {/if}
+    </div>
+
+</div>
+
+
+
+
+<style>
+    table{
+        border-collapse: collapse;
+        width: 40%;
+        margin-top: 20px;
+        margin-inline: auto;
+    }
+    th{
+        border: 1px solid #000;
+        padding: 10px;
+    }
+    tr{
+        border: 1px solid #000;
+        padding: 10px;
+    }
+    td{
+        border: 1px solid black;
+        padding-left: 5px;
+    }
+
+    .main_container{
+        padding-inline: 40px;
+        width: 100%;
+        text-align: center;
+    }
+
+    tr td{
+        padding-block: 10px;
+    }
+
+    .button_container{
+        padding-top: 25px;
+    }
+</style>
